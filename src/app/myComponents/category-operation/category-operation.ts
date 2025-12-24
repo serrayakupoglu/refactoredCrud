cd8df2119a2d25dcf882d736f9f88324ca876e59
Serra <serrayakupoglu@gmail.com>
2025-12-24 14:54:20 +0300

    kategori html eklendi

--- src/app/myComponents/category-operation/category-operation.ts
+++ src/app/myComponents/category-operation/category-operation.ts
@@ -11,6 +11,120 @@ import { toUpdateCategoryRequest,updateCategoryForm } from '../../Validations/ca
   templateUrl: './category-operation.html',
   styleUrl: './category-operation.css',
 })
-export class CategoryOperation {
+export class CategoryOperation implements OnInit {
+  private categoryApi = inject(CategoryApi);
+  protected categories = signal<CategoryResponseModel[]>([]);
+  protected selectedCategory= signal<CategoryResponseModel|null>(null);
 
+  //UI state formları 
+
+  protected createForm = createCategoryForm();
+  protected updateForm = updateCategoryForm();
+
+  private async refreshCategories():Promise<void>{
+    try {
+      const values = await this.categoryApi.getAll();
+      this.categories.set(values); 
+    } catch (error) {
+      console.log("Kategori listesi alınamadı",error);
+      
+    }
+  }
+
+  async ngOnInit():Promise<void> {
+    await this.refreshCategories();
+      
+  }
+
+  //create işlemleri 
+  async onCreate():Promise<void>{
+    if(this.createForm.invalid){
+      this.createForm.markAllAsTouched();
+      return;
+    }
+
+    const req = toCreateCategoryRequest(this.createForm); 
+    await this.categoryApi.create(req); 
+    this.createForm.reset();
+    await this.refreshCategories(); 
+  }
+
+  //update işlemleri 
+  startUpdate(cat:CategoryResponseModel){
+    this.selectedCategory.set(cat); 
+
+    this.updateForm.patchValue({
+      id:cat.id,
+      name:cat.categoryName,
+      description:cat.description,
+    },
+    {emitEvent:false}
+  );
+  }
+  cancelUpdate(){
+    this.selectedCategory.set(null); 
+    this.updateForm.reset({id:0,name:'',description:''}); 
+  }
+
+  async onUpdate(){
+    if(this.updateForm.invalid){
+      this.updateForm.markAllAsTouched();
+      return;
+    }
+    const req = toUpdateCategoryRequest(this.updateForm);
+    await this.categoryApi.update(req); 
+    this.cancelUpdate();
+    await this.refreshCategories(); 
+  }
+
+  //Delete 
+  async onDelete(id:number):Promise<void>{
+    const confirmDelete = window.confirm(`Id'si ${id} olan kategoriyi silmek istediğinize emin misiniz`); 
+    if(!confirmDelete) return;
+
+    try {
+      const message = await this.categoryApi.deleteById(id);
+      console.log('Delete mesajı', message); 
+      this.categories.update((x)=>x.filter((c)=>c.id!==id)); 
+      const selected = this.selectedCategory();
+
+      if(selected && selected.id == id){
+        this.selectedCategory.set(null);
+      }
+    } catch (error) {
+      console.log(error);     
+    }
+  }
+
+  protected labels : Record<string,string> = {
+    name:'Kategori Adı',
+    description: 'Açıklama',
+    id:'Id',
+  }; 
+
+  protected getErrorMessage(control: AbstractControl|null,label='Bu alan'):string|null{
+    if(!control || (!control.touched && control.dirty)||!control.invalid) return null; 
+    else if (control.hasError('required')) return `${label} zorunludur.`;
+
+    else if(control.hasError('minLength')){
+      const e = control.getError('minLength'); 
+
+      return `${label} en az ${e.requiredlength} karakter olmalıdır`; 
+
+    }
+    else if(control.hasError('maxLength')){
+      const e = control.getError('maxLength'); 
+
+      return `${label} en fazla ${e.requiredlength} karakter olmalıdır`; 
+
+    }
+    return `${label} geçersiz`;
+  }
+
+  protected getErrorMessageByName(form:{controls:Record<string,AbstractControl>}, controlName:string):string|null{
+    const control = form.controls[controlName]; 
+    const label = this.labels[controlName] ?? controlName; 
+
+    return this.getErrorMessage(control,label); 
+  }
 }
